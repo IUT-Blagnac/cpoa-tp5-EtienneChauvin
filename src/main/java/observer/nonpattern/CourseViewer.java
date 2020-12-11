3d6e6f7484082ffe50c1f49c479a6e7ee1345448
etiennechauvin <etiennechauvin07@gmail.com>
2020-12-11 22:50:54 +0100

    fix #1.1: Add Class Diagrams

--- src/main/java/observer/nonpattern/CourseViewer.java
+++ src/main/java/observer/nonpattern/CourseViewer.java
@@ -27,176 +27,180 @@ import observer.LayoutConstants;
  * used in this program.
  */
 @SuppressWarnings("serial")
-public class CourseViewer extends JFrame implements ActionListener,
-		ChangeListener {
+public class CourseViewer extends JFrame implements ActionListener, ChangeListener {
 
-	/**
-	 * Create a CourseViewer object
-	 */
-	public CourseViewer() {
-		this.setTitle("Observer Pattern -- Non Pattern Version");
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		sliders = new Vector<JSlider>();
-		sliderPanel = new JPanel();
-		sliderPanel.setLayout(new GridBagLayout());
-		sliderPanel.setBackground(Color.white);
-		this.getContentPane().setLayout(new GridBagLayout());
-		coursePanel = new JPanel();
-		coursePanel.setBorder(new TitledBorder("Courses"));
-		coursePanel.setLayout(new GridLayout(0, 1));
-		GridBagConstraints constraints = new GridBagConstraints();
-		constraints.fill = GridBagConstraints.BOTH;
+    // Frame contents
+    private final JPanel sliderPanel;
 
-		scrollPane = new JScrollPane(coursePanel,
-				JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
-				JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-		// Layout code
-		constraints.weightx = 0.0;
-		constraints.weighty = 1.0;
-		constraints.gridx = 0;
-		constraints.gridy = 0;
-		sliderPanel.add(scrollPane, constraints);
+    private final JPanel coursePanel;
 
-		button = new JButton("New Course");
-		button.addActionListener(this);
+    private final Vector<JSlider> sliders;
 
-		// Layout code
-		constraints.weightx = 0.0;
-		constraints.weighty = 0.0;
-		constraints.gridy = 1;
-		sliderPanel.add(button, constraints);
+    private final JButton button;
 
-		// Layout code
-		constraints.weightx = 0.0;
-		constraints.weighty = 0.0;
-		constraints.gridx = 0;
-		constraints.gridy = 0;
-		this.getContentPane().add(sliderPanel, constraints);
+    /**
+     * Create a CourseViewer object
+     */
+    public CourseViewer() {
+        this.setTitle("Observer Pattern -- Non Pattern Version");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        sliders = new Vector<JSlider>();
+        sliderPanel = new JPanel();
+        sliderPanel.setLayout(new GridBagLayout());
+        sliderPanel.setBackground(Color.white);
+        this.getContentPane().setLayout(new GridBagLayout());
+        coursePanel = new JPanel();
+        coursePanel.setBorder(new TitledBorder("Courses"));
+        coursePanel.setLayout(new GridLayout(0, 1));
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.fill = GridBagConstraints.BOTH;
 
-		// Layout code
-		constraints.weightx = 1.0;
-		constraints.weighty = 1.0;
-		constraints.gridx = 1;
-		constraints.gridy = 0;
-		// the bar chart will be drawn over this panel
-		this.getContentPane().add(new JPanel(), constraints);
-		this.setVisible(true);
-	}
+        JScrollPane scrollPane = new JScrollPane(coursePanel,
+                JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
+                JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        // Layout code
+        constraints.weightx = 0.0;
+        constraints.weighty = 1.0;
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        sliderPanel.add(scrollPane, constraints);
 
-	/**
-	 * Add a new Course object
-	 * 
-	 * @param courseRecord
-	 *            the CourseRecord to be added
-	 */
-	public void addCourse(CourseRecord courseRecord) {
-		boolean alreadyExists = false;
-		for (int i = 0; i < sliders.size(); i++) {
-			JSlider slider = sliders.elementAt(i);
-			if (slider.getName().equals(courseRecord.getName())) {
-				alreadyExists = true;
-				JOptionPane
-						.showMessageDialog(
-								null,
-								"Warning: Attempt to add new course with an already existing name",
-								"alert", JOptionPane.ERROR_MESSAGE);
-				i = sliders.size(); // exit the loop
-			}
-		}
-		if (!alreadyExists) { // add the slider
-			JSlider slider = new JSlider();
-			slider.setBackground(Color.white);
-			slider.setName(courseRecord.getName());
-			slider.setValue(50);
-			slider.setMinimum(0);
-			slider.setMaximum(100);
-			slider.setMajorTickSpacing(25);
-			slider.setMinorTickSpacing(5);
-			slider.setPaintTicks(true);
-			slider.setPaintLabels(true);
-			slider.setBorder(new TitledBorder(courseRecord.getName()));
-			slider.addChangeListener(this);
-			coursePanel.add(slider);
-			coursePanel.revalidate();
-			sliders.addElement(slider);
-			this.setSize(LayoutConstants.xOffset + 50 + this.sliders.size()
-					* (LayoutConstants.barWidth + LayoutConstants.barSpacing),
-					(sliders.size() + 1) * 100 + this.button.getHeight());
-			this.sliderPanel.revalidate();
-			this.coursePanel.revalidate();
-			this.repaint();
-			this.setVisible(true);
-		}
-	}
+        button = new JButton("New Course");
+        button.addActionListener(this);
 
-	public void paint(Graphics g) {
-		super.paint(g);
-		LayoutConstants.paintBarChartOutline(g, sliders.size());
-		for (int i = 0; i < sliders.size(); i++) {
-			JSlider record = sliders.elementAt(i);
-			g.setColor(LayoutConstants.courseColours[i]);
-			g.fillRect(
-					LayoutConstants.xOffset + (i + 1)
-							* LayoutConstants.barSpacing + i
-							* LayoutConstants.barWidth, LayoutConstants.yOffset
-							+ LayoutConstants.graphHeight
-							- LayoutConstants.barHeight + 2
-							* (LayoutConstants.maxValue - record.getValue()),
-					LayoutConstants.barWidth, 2 * record.getValue());
-			g.setColor(Color.red);
-			g.drawString(record.getName(),
-					LayoutConstants.xOffset + (i + 1)
-							* LayoutConstants.barSpacing + i
-							* LayoutConstants.barWidth, LayoutConstants.yOffset
-							+ LayoutConstants.graphHeight + 20);
-		}
-	}
+        // Layout code
+        constraints.weightx = 0.0;
+        constraints.weighty = 0.0;
+        constraints.gridy = 1;
+        sliderPanel.add(button, constraints);
 
-	/**
-	 * Manages the creation of a new course. Called when "New Course" button is pressed.
-	 * 
-	 * @param arg0
-	 *            not used
-	 */
-	public void actionPerformed(ActionEvent arg0) {
-		String input = JOptionPane
-				.showInputDialog("Please enter the new course name:");
-		if (input != null)
-			this.addCourse(new CourseRecord(input, 50));
-	}
+        // Layout code
+        constraints.weightx = 0.0;
+        constraints.weighty = 0.0;
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        this.getContentPane().add(sliderPanel, constraints);
 
-	/**
-	 * Handles the changing of the marks for a course (changing of a JSlider)
-	 * 
-	 * @param arg0
-	 *            not used
-	 */
-	public void stateChanged(ChangeEvent arg0) {
-		this.repaint();
-	}
+        // Layout code
+        constraints.weightx = 1.0;
+        constraints.weighty = 1.0;
+        constraints.gridx = 1;
+        constraints.gridy = 0;
+        // the bar chart will be drawn over this panel
+        this.getContentPane().add(new JPanel(), constraints);
+        this.setVisible(true);
+    }
 
-	
-	/**
-	 * Sets up an initial set of three courses
-	 * 
-	 * @param args
-	 *            not used
-	 */
-	public static void main(String[] args) {
-		CourseViewer viewer = new CourseViewer();
-		viewer.addCourse(new CourseRecord("Physics", 50));
-		viewer.addCourse(new CourseRecord("Chemistry", 50));
-		viewer.addCourse(new CourseRecord("Biology", 50));
-	}
-	
-	// Frame contents
-	private JPanel sliderPanel;
+    /**
+     * Add a new Course object
+     *
+     * @param courseRecord the CourseRecord to be added
+     */
+    public void addCourse(CourseRecord courseRecord) {
+        boolean alreadyExists = false;
+        for (int i = 0; i < sliders.size(); i++) {
+            JSlider slider = sliders.elementAt(i);
+            if (slider.getName().equals(courseRecord.getName())) {
+                alreadyExists = true;
+                JOptionPane
+                        .showMessageDialog(
+                                null,
+                                "Warning: Attempt to add new course with an already existing name",
+                                "alert", JOptionPane.ERROR_MESSAGE);
+                i = sliders.size(); // exit the loop
+            }
+        }
+        if (!alreadyExists) { // add the slider
+            JSlider slider = new JSlider();
+            slider.setBackground(Color.white);
+            slider.setName(courseRecord.getName());
+            slider.setValue(50);
+            slider.setMinimum(0);
+            slider.setMaximum(100);
+            slider.setMajorTickSpacing(25);
+            slider.setMinorTickSpacing(5);
+            slider.setPaintTicks(true);
+            slider.setPaintLabels(true);
+            slider.setBorder(new TitledBorder(courseRecord.getName()));
+            slider.addChangeListener(this);
+            coursePanel.add(slider);
+            coursePanel.revalidate();
+            sliders.addElement(slider);
+            this.setSize(LayoutConstants.xOffset + 50 + this.sliders.size()
+                            * (LayoutConstants.barWidth + LayoutConstants.barSpacing),
+                    (sliders.size() + 1) * 100 + this.button.getHeight());
+            this.sliderPanel.revalidate();
+            this.coursePanel.revalidate();
+            this.repaint();
+            this.setVisible(true);
+        }
+    }
 
-	private JPanel coursePanel;
+    public void paint(Graphics g) {
+        super.paint(g);
+        LayoutConstants.paintBarChartOutline(g, sliders.size());
+        double startAngle = 0.0;
+        double total = 0;
+        for (int i = 0; i < sliders.size(); i++) {
+            total += sliders.get(i).getValue();
+        }
+        for (int i = 0; i < sliders.size(); i++) {
+            JSlider record = sliders.elementAt(i);
+            g.setColor(LayoutConstants.courseColours[i]);
+            final int chrt = LayoutConstants.xOffset + (i + 1)
+                    * LayoutConstants.barSpacing + i
+                    * LayoutConstants.barWidth;
+            g.fillRect(
+                    chrt, LayoutConstants.yOffset
+                            + LayoutConstants.graphHeight
+                            - LayoutConstants.barHeight + 2
+                            * (LayoutConstants.maxValue - record.getValue()),
+                    LayoutConstants.barWidth, 2 * record.getValue());
+            //Pie chart
+            System.out.println(sliders.size());
+            double ratio = ((double) record.getValue() / total) * 360.0;
+            int radius = 50;
+            g.fillArc(LayoutConstants.xOffset, LayoutConstants.yOffset + 300, 2 * radius, 2 * radius, (int) startAngle, (int) ratio);
+            g.setColor(Color.red);
+            startAngle += ratio;
 
-	private Vector<JSlider> sliders;
+            g.drawString(record.getName(),
+                    chrt, LayoutConstants.yOffset
+                            + LayoutConstants.graphHeight + 20);
+        }
+    }
 
-	private JScrollPane scrollPane;
+    /**
+     * Manages the creation of a new course. Called when "New Course" button is pressed.
+     *
+     * @param arg0 not used
+     */
+    public void actionPerformed(ActionEvent arg0) {
+        String input = JOptionPane
+                .showInputDialog("Please enter the new course name:");
+        if (input != null)
+            this.addCourse(new CourseRecord(input, 50));
+    }
 
-	private JButton button;
+    /**
+     * Handles the changing of the marks for a course (changing of a JSlider)
+     *
+     * @param arg0 not used
+     */
+    public void stateChanged(ChangeEvent arg0) {
+        this.repaint();
+    }
+
+
+    /**
+     * Sets up an initial set of three courses
+     *
+     * @param args not used
+     */
+    public static void main(String[] args) {
+        CourseViewer viewer = new CourseViewer();
+        viewer.addCourse(new CourseRecord("Physics", 50));
+        viewer.addCourse(new CourseRecord("Chemistry", 50));
+        viewer.addCourse(new CourseRecord("Biology", 50));
+    }
 }
--- src/main/java/observer/pattern/CourseController.java
+++ src/main/java/observer/pattern/CourseController.java
@@ -6,6 +6,7 @@ import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import java.util.Vector;
 
 import javax.swing.JButton;
@@ -26,154 +27,159 @@ import observer.CourseRecord;
  */
 @SuppressWarnings("serial")
 public class CourseController extends JPanel implements Observer, ChangeListener, ActionListener {
-	/**
-	 * Constructs a CourseController object
-	 * 
-	 * @param courses
-	 *            a set of courses and their marks
-	 */
-	public CourseController(CourseData courses) {
-		this.courseData = courses;
-		this.sliders = new Vector<JSlider>();
-		this.setLayout(new GridBagLayout());
-		this.setBackground(Color.white);
 
-		coursePanel = new JPanel();
-		coursePanel.setBorder(new TitledBorder("Courses"));
-		coursePanel.setLayout(new GridLayout(0, 1));
+    private final CourseData courseData;
 
-		GridBagConstraints constraints = new GridBagConstraints();
-		constraints.fill = GridBagConstraints.BOTH;
-		courses.attach(this);
-		Vector<CourseRecord> state = courses.getUpdate();
+    private final Vector<JSlider> sliders;
 
-		for (int i = 0; i < state.size(); i++)
-			this.addCourse(state.elementAt(i));
+    private final JPanel coursePanel;
 
-		JScrollPane scrollPane = new JScrollPane(coursePanel,
-				JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
-				JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-		JButton button = new JButton("New Courses");
-		button.addActionListener(this);
+    private static final ArrayList<Observer> observers = new ArrayList<>();
 
-		constraints.weightx = 0.5;
-		constraints.weighty = 1.0;
-		constraints.gridx = 0;
-		constraints.gridy = 0;
-		this.add(scrollPane, constraints);
+    /**
+     * Constructs a CourseController object
+     *
+     * @param courses a set of courses and their marks
+     */
+    public CourseController(CourseData courses) {
+        this.courseData = courses;
+        this.sliders = new Vector<JSlider>();
+        this.setLayout(new GridBagLayout());
+        this.setBackground(Color.white);
 
-		constraints.weightx = 0.5;
-		constraints.weighty = 0;
-		constraints.gridy = 1;
-		this.add(button, constraints);
-	}
+        coursePanel = new JPanel();
+        coursePanel.setBorder(new TitledBorder("Courses"));
+        coursePanel.setLayout(new GridLayout(0, 1));
 
-	/**
-	 * Add a new course
-	 * 
-	 * @param record
-	 *            the new course record to be added
-	 */
-	public void addCourse(CourseRecord record) {
-		JSlider slider = new JSlider();
-		slider.setBackground(Color.white);
-		slider.setName(record.getName());
-		slider.setValue(record.getNumOfStudents());
-		slider.setMinimum(0);
-		slider.setMaximum(100);
-		slider.setMajorTickSpacing(25);
-		slider.setMinorTickSpacing(5);
-		slider.setPaintTicks(true);
-		slider.setPaintLabels(true);
-		slider.setBorder(new TitledBorder(record.getName()));
-		slider.addChangeListener(this);
-		coursePanel.add(slider);
-		coursePanel.revalidate();
-		sliders.addElement(slider);
-	}
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.fill = GridBagConstraints.BOTH;
+        courses.attach(this);
+        Vector<CourseRecord> state = courses.getUpdate();
 
-	/**
-	 * Informs this CourseController that a new course has been added
-	 * 
-	 * @param o
-	 *            the CourseData subject that has changed
-	 */
-	 public void update(Observable o) {
-		CourseData courses = (CourseData) o;
-		Vector<CourseRecord> newCourses = courses.getUpdate();
-		for (int i = sliders.size(); i < newCourses.size(); i++) {
-			this.addCourse((CourseRecord) newCourses.elementAt(i));
-		}
-	} 
+        for (int i = 0; i < state.size(); i++)
+            this.addCourse(state.elementAt(i));
 
-	/**
-	 * Manages the creation of a new course. Called when the "New Course" button is pressed.
-	 * 
-	 * @param arg0
-	 *            not used
-	 */
-	public void actionPerformed(ActionEvent arg0) {
-		String input = JOptionPane.showInputDialog("Please enter new course name:");
-		if (input != null){
-			courseData.addCourseRecord(new CourseRecord(input, 50));
-			// leave it up notify/update mechanism to invoke this.addCourse
-		}
-	}
+        JScrollPane scrollPane = new JScrollPane(coursePanel,
+                JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
+                JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        JButton button = new JButton("New Courses");
+        button.addActionListener(this);
 
-	/**
-	 * Handles the changing of the marks for a course (changing of a JSlider)
-	 * 
-	 * @param arg0
-	 *            the JSlider that has changed
-	 */
-	public void stateChanged(ChangeEvent arg0) {
-		JSlider slider = (JSlider) arg0.getSource();
-		courseData.changeCourseRecord(slider.getName(), slider.getValue());
-	}
+        constraints.weightx = 0.5;
+        constraints.weighty = 1.0;
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        this.add(scrollPane, constraints);
 
-	/**
-	 * Sets up an initial set of three courses
-	 * 
-	 * @param args
-	 *            not used
-	 */
-	public static void main(String[] args) {
-		CourseData data = new CourseData();
-		data.addCourseRecord(new CourseRecord("Physics", 50));
-		data.addCourseRecord(new CourseRecord("Chemistry", 50));
-		data.addCourseRecord(new CourseRecord("Biology", 50));
+        constraints.weightx = 0.5;
+        constraints.weighty = 0;
+        constraints.gridy = 1;
+        this.add(button, constraints);
+    }
 
-		CourseController controller = new CourseController(data);
-		BarChartObserver bar = new BarChartObserver(data);
+    /**
+     * Add a new course
+     *
+     * @param record the new course record to be added
+     */
+    public void addCourse(CourseRecord record) {
+        JSlider slider = new JSlider();
+        slider.setBackground(Color.white);
+        slider.setName(record.getName());
+        slider.setValue(record.getNumOfStudents());
+        slider.setMinimum(0);
+        slider.setMaximum(100);
+        slider.setMajorTickSpacing(25);
+        slider.setMinorTickSpacing(5);
+        slider.setPaintTicks(true);
+        slider.setPaintLabels(true);
+        slider.setBorder(new TitledBorder(record.getName()));
+        slider.addChangeListener(this);
+        coursePanel.add(slider);
+        coursePanel.revalidate();
+        sliders.addElement(slider);
+    }
 
-		JScrollPane scrollPane = new JScrollPane(bar,
-				JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
-				JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+    /**
+     * Informs this CourseController that a new course has been added
+     *
+     * @param o the CourseData subject that has changed
+     */
+    public void update(Observable o, Vector<CourseRecord> courseData) {
+        for (int i = sliders.size(); i < courseData.size(); i++) {
+            this.addCourse((CourseRecord) courseData.elementAt(i));
+        }
+    }
 
-		JFrame frame = new JFrame("Observer Pattern");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(new GridBagLayout());
-		frame.setResizable(false);
+    /**
+     * Manages the creation of a new course. Called when the "New Course" button is pressed.
+     *
+     * @param arg0 not used
+     */
+    public void actionPerformed(ActionEvent arg0) {
+        String input = JOptionPane.showInputDialog("Please enter new course name:");
+        if (input != null) {
+            courseData.addCourseRecord(new CourseRecord(input, 50), observers);
+            // leave it up notify/update mechanism to invoke this.addCourse
+        }
+    }
 
-		GridBagConstraints constraints = new GridBagConstraints();
-		constraints.fill = GridBagConstraints.BOTH;
-		constraints.weightx = 0.25;
-		constraints.weighty = 1.0;
-		constraints.gridx = 0;
-		constraints.gridy = 0;
-		frame.getContentPane().add(controller, constraints);
-		constraints.weightx = 0.5;
-		constraints.weighty = 1.0;
-		constraints.gridx = 1;
-		constraints.gridy = 0;
-		frame.getContentPane().add(scrollPane, constraints);
-		frame.pack();
-		frame.setVisible(true);
-	}
+    /**
+     * Handles the changing of the marks for a course (changing of a JSlider)
+     *
+     * @param arg0 the JSlider that has changed
+     */
+    public void stateChanged(ChangeEvent arg0) {
+        JSlider slider = (JSlider) arg0.getSource();
+        courseData.changeCourseRecord(slider.getName(), slider.getValue());
+    }
 
-	private CourseData courseData;
+    /**
+     * Sets up an initial set of three courses
+     *
+     * @param args not used
+     */
+    public static void main(String[] args) {
+        CourseData data = new CourseData();
+        data.addCourseRecord(new CourseRecord("Physics", 50), null);
+        data.addCourseRecord(new CourseRecord("Chemistry", 50), null);
+        data.addCourseRecord(new CourseRecord("Biology", 50), null);
 
-	private Vector<JSlider> sliders;
+        CourseController controller = new CourseController(data);
+        BarChartObserver bar = new BarChartObserver(data);
+        PyChartObserver pychart = new PyChartObserver(data);
 
-	private JPanel coursePanel;
+        observers.add(bar);
+        observers.add(pychart);
+        observers.add(controller);
+
+        JScrollPane scrollPane = new JScrollPane(bar,
+                JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+                JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+
+        JFrame frame = new JFrame("Observer Pattern");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().setLayout(new GridBagLayout());
+        frame.setResizable(false);
+
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.fill = GridBagConstraints.BOTH;
+        constraints.weightx = 0.1;
+        constraints.weighty = 1;
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        frame.getContentPane().add(controller, constraints);
+        constraints.weightx = 1;
+        constraints.weighty = 1.0;
+        constraints.gridx = 1;
+        constraints.gridy = 0;
+        frame.getContentPane().add(scrollPane, constraints);
+        constraints.weightx = 1;
+        constraints.weighty = 1.0;
+        constraints.gridx = 1;
+        constraints.gridy = 1;
+        frame.getContentPane().add(pychart, constraints);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
